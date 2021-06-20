c69e89b73bf536a177b9f3d3f9551945ebab85ea
Kamran <mohd.kamran@tothenew.com>
2021-06-20 15:31:29 +0530

    Stacks: Infix to Postfix

---
+++ ElementaryDS/Stacks/infix2postfix.js
@@ -0,0 +1,49 @@
+
+
+// let infix = '(a+b*c)-(c/d)';
+// Assumption: The string is correct and balanced
+let infix = 'a*b+c*d';
+
+let s = []; // Final result
+let op = []; // operatoror stack
+
+// Function to check if operand op1 has higher precedence than op2
+const operators = ['+', '-', '*', '/', '^', '('];
+const precedence = (op1, op2) => operators.indexOf(op1) >= operators.indexOf(op2);
+
+
+const postfix = (A) => {
+    // step-1: Enclose the string in ()
+    A = '(' + A + ')';
+    // step-2 Iterate over the string
+    for (let i=0; i<A.length; i++) {
+        const char = A[i];
+        console.log(A[i], op, s);
+        if (operators.includes(char)) {
+            // Push all operators in op to s that has higher or equal precedence than current operator
+            let top = op.length-1;
+            while (op[top] !== '(' && top >= 0) {
+                if(precedence(op[top], char)) s.push(op.pop());
+                top--;
+            }
+            // Why are we popping any higher operators from op?
+            // Finally push the current operator onto op stack
+            op.push(A[i]);
+        }
+        else if (char === ')') {
+            // If a ) is encountered 
+            let top = op.length-1;
+            while (op[top] !== '(' && top >= 0) {
+                s.push(op.pop());
+                top--;
+            }
+            op.pop();
+        }
+        else {
+            s.push(A[i]);
+        }
+    }
+}
+
+postfix(infix);
+console.log('Postfix: ', s.join(''));
--- ElementaryDS/Stacks/usingClass.js
+++ ElementaryDS/Stacks/usingClass.js
@@ -41,5 +41,56 @@ ob.pop();
 ob.pop();
 
 
+//Alternative Syntax: Stack below is still a Class
+// function Stack(capacity) {
+//     if (capacity === undefined) { capacity = 1000; }
+//     this.top = -1;
+//     this.data = new Array(capacity);
+// }
+
+// Stack.prototype.size = function() {
+//     return (this.top + 1);
+// };
+
+// Stack.prototype.isEmpty = function() {
+//     return (this.top === -1);
+// };
+
+// Stack.prototype.push = function(value) {
+//     this.top++;
+//     this.data[this.top] = value;
+// };
+
+// Stack.prototype.top = function() {
+//     if (this.isEmpty()) {
+//         throw new Error("StackEmptyException");
+//     }
+//     return this.data[this.top];
+// };
+
+// Stack.prototype.pop = function() {
+//     if (this.isEmpty()) {
+//         throw new Error("StackEmptyException");
+//     }
+//     var topVal = this.data[this.top];
+//     this.top--;
+//     return topVal;
+// };
+
+// Stack.prototype.print = function() {
+//     for (var i = this.top; i >= 0; i--) {
+//         console.log(this.data[i]);
+//     }
+// };
+
+// var s = new Stack();
+// s.push(1);
+// s.push(2);
+// s.push(3);
+// s.print();
+// console.info(s.pop());
+// console.info(s.pop());
+
+
 
 
